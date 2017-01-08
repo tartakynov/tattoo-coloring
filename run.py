da065a42c1eec51a02e924a2d687fa72a44406e0
Artem Tartakynov <artem.tartakynov@agoda.com>
2017-01-08 14:55:24 +0700

    added control buttons

--- run.py
+++ run.py
@@ -5,32 +5,47 @@ from PIL import Image, ImageTk
 from itertools import cycle
 
 class App(Frame):
-    def __init__(self, master, delay, images):
-        Frame.__init__(self, master)
-        self.columnconfigure(0, weight = 1)
-        self.rowconfigure(0, weight = 1)
-        self.delay = delay
-        self.size = (master.winfo_width(), master.winfo_height())
-        self.images = cycle(images)
-        self.display = Canvas(self, bd = 0, highlightthickness = 0)
-        self.display.grid(row = 0, sticky = W + E + N + S)
-        self.pack(fill = BOTH, expand = 1)
+    def __init__(self, master, images, size, **args):
+        Frame.__init__(self, master, **args)
+        self.pos = 0
+        self.size = size
+        self.images = images
+        self.make_widgets()
+        self.pack(expand = YES, fill = BOTH)
 
-    def next_slide(self):
-        original = next(self.images)
+    def make_widgets(self):
+        width, height = self.size
+        self.canvas = Canvas(self, bg = 'white', height = height, width = width)
+        self.canvas.pack(side = LEFT, fill = BOTH, expand = YES)
+        Button(self, text = 'Next', command = self.on_next).pack(fill = X)
+        Button(self, text = 'Prev', command = self.on_prev).pack(fill = X)
+        Button(self, text = 'Quit', command = self.on_quit).pack(fill = X)
+
+    def redraw(self):
+        original = self.images[self.pos]
         self.image = ImageTk.PhotoImage(original.resize(self.size, Image.ANTIALIAS))
-        self.display.delete("IMG")
-        self.display.create_image(0, 0, image = self.image, anchor = NW, tags = "IMG")
-        self.after(self.delay, self.next_slide)
+        self.canvas.delete("IMG")
+        self.canvas.create_image(2, 2, image = self.image, anchor = NW, tags = "IMG")
+
+    def on_next(self):
+        self.pos = min(len(self.images) - 1, self.pos + 1)
+        self.redraw()
+
+    def on_prev(self):
+        self.pos = max(0, self.pos - 1)
+        self.redraw()
+
+    def on_quit(self):
+        self.update()
+        self.quit()
 
     def run(self):
-        self.next_slide()
+        self.redraw()
         self.mainloop()
 
 if __name__ == "__main__":
+    size = (1024, 768)
     root = Tk()
-    root.geometry('1024x768')
-    root.update()
-    images = [Image.open('tattoo.jpg')]
-    app = App(root, 3000, images)
+    images = [Image.open('tattoo.jpg'), Image.open('studio.jpg')]
+    app = App(root, images = images, size = size, bd = 3, relief = SUNKEN)
     app.run()
