f31f78f44bd0d954481c2ccf38698fff351ba7ca
Artem Tartakynov <artem.tartakynov@agoda.com>
2017-01-08 13:17:41 +0700

    slideshow

--- run.py
+++ run.py
@@ -2,30 +2,35 @@
 
 from Tkinter import *
 from PIL import Image, ImageTk
+from itertools import cycle
 
 class App(Frame):
-    def __init__(self, master):
+    def __init__(self, master, delay, images):
         Frame.__init__(self, master)
         self.columnconfigure(0, weight = 1)
         self.rowconfigure(0, weight = 1)
-        self.original = Image.open('tattoo.jpg')
-        self.image = ImageTk.PhotoImage(self.original)
+        self.delay = delay
+        self.size = (master.winfo_width(), master.winfo_height())
+        self.images = cycle(images)
         self.display = Canvas(self, bd = 0, highlightthickness = 0)
-        self.display.create_image(0, 0, image = self.image, anchor = NW, tags = "IMG")
         self.display.grid(row = 0, sticky = W + E + N + S)
         self.pack(fill = BOTH, expand = 1)
-        self.bind("<Configure>", self.resize)
 
-    def resize(self, event):
-        size = (event.width, event.height)
-        resized = self.original.resize(size, Image.ANTIALIAS)
-        self.image = ImageTk.PhotoImage(resized)
+    def next_slide(self):
+        original = next(self.images)
+        self.image = ImageTk.PhotoImage(original.resize(self.size, Image.ANTIALIAS))
         self.display.delete("IMG")
         self.display.create_image(0, 0, image = self.image, anchor = NW, tags = "IMG")
+        self.after(self.delay, self.next_slide)
+
+    def run(self):
+        self.next_slide()
+        self.mainloop()
 
 if __name__ == "__main__":
     root = Tk()
-    root.geometry('1024x960')
-    app = App(root)
-    app.mainloop()
-    root.destroy()
+    root.geometry('1024x768')
+    root.update()
+    images = [Image.open('tattoo.jpg')]
+    app = App(root, 3000, images)
+    app.run()
