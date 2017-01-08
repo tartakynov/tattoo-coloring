d356e2b0851df1ba860030248682fa9d9ce04cfd
Artem Tartakynov <artem.tartakynov@agoda.com>
2017-01-08 12:54:09 +0700

    show image with tkinter

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
+from Tkinter import *
+from PIL import ImageTk, Image
+import os
+
 if __name__ == "__main__":
-    pass
+    root = Tk()
+    img = ImageTk.PhotoImage(Image.open("tattoo.jpg"))
+    imglabel = Label(root, image = img).grid(row = 1, column = 1)
+    root.mainloop()
